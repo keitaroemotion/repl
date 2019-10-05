dda950d5c47bcb29d1134b07688174feff6393ee
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-10-05 20:00:41 +0900

    XXX

--- src/Main.purs
+++ src/Main.purs
@@ -1,11 +1,10 @@
 module Main where
 
+import Effect         (Effect)
+import Effect.Aff     (Aff)
 import Effect.Console (log)
 import Data.List      (List, (:))
 import Data.List      as List
-import Effect         (Effect)
-import Effect.Aff     (Aff)
-import Effect.Aff as Aff
 import Prelude
 
 foreign import argv :: Array String
